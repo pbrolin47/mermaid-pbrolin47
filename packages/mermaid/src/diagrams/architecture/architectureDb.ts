de666c7f91b407d434838c53e70aa327cb9528f7
Per Brolin <per@mermaidchart.com>
2026-04-23 14:45:12 +0200

    Removed comment

--- packages/mermaid/src/diagrams/architecture/architectureDb.ts
+++ packages/mermaid/src/diagrams/architecture/architectureDb.ts
@@ -34,7 +34,6 @@ import {
   shiftPositionByArchitectureDirectionPair,
 } from './architectureTypes.js';
 
-// Added comment
 const DEFAULT_ARCHITECTURE_CONFIG: Required<ArchitectureDiagramConfig> =
   DEFAULT_CONFIG.architecture;
 export class ArchitectureDB implements DiagramDB {
