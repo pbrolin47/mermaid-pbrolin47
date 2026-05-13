f858909c4de00d987a848d87f9be755042a562fd
Per Brolin <per@mermaidchart.com>
2026-05-13 15:11:28 +0200

    Dummy test

--- packages/mermaid/src/diagrams/block/blockDB.ts
+++ packages/mermaid/src/diagrams/block/blockDB.ts
@@ -23,6 +23,7 @@ let diagramId = '';
 
 const sanitizeText = (txt: string) => common.sanitizeText(txt, config);
 
+// Added comment for test
 /**
  * Called when the parser comes across a (style) class definition
  * @example classDef my-style fill:#f96;
